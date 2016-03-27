9ce4ce4f07b2ed4ae7e82821be7167ac3a54d17b
lujjjh <i@codehut.me>
2016-03-27 14:51:12 +0800

    export environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,9 @@ fi
 : ${DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
 : ${DB_NAME:=$MYSQL_ENV_MYSQL_DATABASE}
 
+export DB_HOST=$DB_HOST
+export DB_PORT=$DB_PORT
+export DB_USER=$DB_USER
+export DB_NAME=$DB_NAME
+
 exec "$@"
